8d29c917fe4d5915333e6c3481161b1baa1a482c
vincentchu <vince@posterous.com>
2011-09-19 01:06:05 -0700

    Switching back to using non global variable

--- ext/duple_index/DupleIndex.c
+++ ext/duple_index/DupleIndex.c
@@ -15,8 +15,18 @@ void Init_duple_index() {
 
 VALUE method_alloc_index(VALUE self) {
   printf("\n\n**** method_alloc_index\n");
-  int *derp;
-  return Data_Wrap_Struct(self, NULL, method_free_index, derp);
+  struct duples_hash *duples, *ptr;
+
+  // Initialize the hash with a single, hidden entry so that
+  // the *duples pointer doesn't point to NULL
+  n_malloc_calls++;
+  ptr          = malloc(sizeof(struct duples_hash));
+  ptr->id      = duple_id(-1, -1);
+  ptr->strings = create_duple_pos(-1, -1, NULL, NULL);
+
+  HASH_ADD_INT(duples, id, ptr);
+
+  return Data_Wrap_Struct(self, NULL, method_free_index, duples);
 }
 
 VALUE method_free_index(VALUE self) {
@@ -29,64 +39,46 @@ VALUE method_free_index(VALUE self) {
 VALUE method_add(VALUE self, VALUE r_str_id, VALUE r_str) {
   printf("\n\n**** method_add\n");
   int i, str_id, str_len, c_a, c_b;
+  struct duples_hash *duples;
 
+  Data_Get_Struct(self, struct duples_hash, duples);
   str_len = (int) RARRAY_LEN(r_str);
   str_id  = NUM2INT( r_str_id );
 
   for (i=0; i<(str_len-1); i++) {
     c_a = NUM2INT( RARRAY_PTR(r_str)[i] );
     c_b = NUM2INT( RARRAY_PTR(r_str)[i+1] );
-    add_duple(c_a, c_b, str_id, i);
-
-    struct duples_hash *foo;
-    long int foo_id;
-
-    foo_id = duple_id(c_a, c_b);
-    HASH_FIND_INT(duples, &foo_id, foo);
-
-    if (foo == NULL) {
-      printf("YYY %d (%d, %d) MISS\n", foo_id, c_a, c_b);
-    } else {
-      printf("yyy %d (%d, %d) HIT\n", foo_id, c_a, c_b);
-    }
+    add_duple(duples, c_a, c_b, str_id, i);
   }
-
 
   return Qtrue;
 }
 
 VALUE method_query(VALUE self, VALUE r_a, VALUE r_b) {
   printf("\n\n**** method_query\n");
-  struct duples_hash *ptr;
+  struct duples_hash *duples, *ptr;
   struct duple_pos *pos;
   int c_a, c_b;
+
+  Data_Get_Struct(self, struct duples_hash, duples);
 
   c_a = NUM2INT( r_a );
   c_b = NUM2INT( r_b );
 
-  printf("---%d %d\n", c_a, c_b);
+  ptr = duple_at(duples, c_a, c_b);;
+  if (ptr != NULL) {
+    pos = ptr->strings;
 
-  ptr = duple_at(c_a, c_b);
-  pos = ptr->strings;
+     while(1) {
 
-  if (ptr == NULL) {
-    printf("NULL\n");
-  } else {
-    printf("NOT NULL\n");
+      printf("duple (%3d, %3d) found in string %5d at position %5d\n", c_a, c_b, pos->index, pos->pos);
+
+      if (pos->next == NULL)
+        break;
+
+      pos = pos->next;
+    }
   }
-
-
-
-
-  /*  while(1) {*/
-
-  /*   printf("duple (%3d, %3d) found in string %5d at position %5d\n", c_a, c_b, pos->index, pos->pos);*/
-
-  /*   if (pos->next == NULL)*/
-  /*     break;*/
-
-  /*   pos = pos->next;*/
-  /* }*/
 
   return Qtrue;
 }
@@ -95,7 +87,7 @@ int duple_id(int c_a, int c_b) {
  return c_b + (c_a % MAX_CHARS) * MAX_CHARS;
 }
 
-struct duples_hash *duple_at(int c_a, int c_b) {
+struct duples_hash *duple_at(struct duples_hash *duples, int c_a, int c_b) {
   int d_id;
   struct duples_hash *d;
 
@@ -105,11 +97,11 @@ struct duples_hash *duple_at(int c_a, int c_b) {
   return d;
 }
 
-void add_duple(int c_a, int c_b, int index, int pos) {
+void add_duple(struct duples_hash *duples, int c_a, int c_b, int index, int pos) {
   struct duples_hash *ptr;
   struct duple_pos *d_pos;
 
-  ptr = duple_at(c_a, c_b);
+  ptr = duple_at(duples, c_a, c_b);
   if (ptr == NULL) {
     printf("duple %5d (%3d, %3d) MISS!\n", duple_id(c_a, c_b), c_a, c_b);
 
@@ -127,20 +119,6 @@ void add_duple(int c_a, int c_b, int index, int pos) {
     ptr->strings->prev = d_pos;
     ptr->strings       = d_pos;
   }
-
-  struct duples_hash *foo;
-  int foo_id;
-
-  foo_id = duple_id(c_a, c_b);
-  HASH_FIND_INT(duples, &foo_id, foo);
-
-  if (foo == NULL) {
-    printf("XXX %d (%d, %d) MISS\n", foo_id, c_a, c_b);
-  } else {
-    printf("XXX %d (%d, %d) HIT\n", foo_id, c_a, c_b);
-  }
-
-
 }
 
 struct duple_pos *create_duple_pos(int index, int pos, struct duple_pos *next, struct duple_pos *prev) {
