7c23472f59b4a2a4369f04bfbc1c76561523b29c
vincentchu <vince@posterous.com>
2011-10-04 01:05:00 -0700

    Getting rid of logging statements

--- ext/duple_index/DupleIndex.c
+++ ext/duple_index/DupleIndex.c
@@ -78,8 +78,6 @@ VALUE method_add(VALUE self, VALUE r_str_id, VALUE r_str) {
 
 VALUE method_match(VALUE self, VALUE needle) {
 
-  //printf("\nmethod_match\n");
-
   int i, n_needle, c_a, c_b, match_id;
   struct match *matches, *match, *match_tmp;
   struct duples_hash *duples, *duple;
@@ -99,14 +97,11 @@ VALUE method_match(VALUE self, VALUE needle) {
     c_a = NUM2INT( RARRAY_PTR(needle)[i] );
     c_b = NUM2INT( RARRAY_PTR(needle)[i+1] );
 
-    //printf("Examining duple %d, %d\n", c_a, c_b);
     duple = duple_at(duples, c_a, c_b);
 
     if (duple != NULL ) {
       pos = duple->strings;
       while (1) {
-
-        //printf("  Found in string %d at pos %d\n", pos->index, pos->pos);
 
         // if String not found in matches, create new pointer, otherwise
         // append count/position if appropriate
@@ -114,7 +109,6 @@ VALUE method_match(VALUE self, VALUE needle) {
         HASH_FIND_INT(matches, &match_id, match);
 
         if (match == NULL) {
-          //printf("  first time matching, creating\n");
           match = malloc( sizeof(struct match) );
 
           match->id = match_id;
@@ -126,7 +120,6 @@ VALUE method_match(VALUE self, VALUE needle) {
           HASH_ADD_INT(matches, id, match);
 
         } else {
-          //printf("  string previously found\n");
           if ((match->last_matched_position < pos->pos) && (match->last_matched_ca != c_a) && (match->last_matched_cb != c_b)) {
             match->n_matches++;
             match->last_matched_position = pos->pos;
@@ -140,26 +133,16 @@ VALUE method_match(VALUE self, VALUE needle) {
 
         pos = pos->next;
       }
-    } else {
-      //printf("  Duple not found!\n");
     }
   }
 
   // Loop over hash entries and spit out results
   HASH_ITER(hh, matches, match, match_tmp) {
-    //printf("Match: index %d, %d matches\n", match->id, match->n_matches);
-
     arr = rb_hash_aref(matches_by_score, INT2NUM(match->n_matches));
-    if (arr == Qnil) {
-      //printf("NIL!\n");
+    if (arr == Qnil)
       arr = rb_ary_new();
-      rb_ary_push(arr, INT2NUM(match->id));
 
-    } else {
-      //printf("NOT NIL\n");
-      rb_ary_push(arr, INT2NUM(match->id));
-    }
-
+    rb_ary_push(arr, INT2NUM(match->id));
     rb_hash_aset(matches_by_score, INT2NUM(match->n_matches), arr);
     HASH_DEL(matches, match);
     free(match);
